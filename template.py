840a70293aad51939e9ebf1c2698ef5e35e119ff
pratham <prathambattamwar1@gmail.com>
2025-07-30 21:53:29 +0530

    folder structure

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = "mlProject"
 
 
-list_of_files = {
+list_of_files = [ 
     ".github/workflow/.gitkeep",
     f"src/{project_name}/init.py",
 f"src/{project_name}/components/_init.py",
@@ -28,7 +28,8 @@ f"src/{project_name}/constants/init.py",
 "setup.py",
 "research/trials.ipynb",
 "templates/index.html"
-}
+]
+
 
 for filepath in list_of_files:
     filepath= Path(filepath)
@@ -36,4 +37,11 @@ for filepath in list_of_files:
 
 
     if filedir !="":
-        os.makedirs(filedir , )
+        os.makedirs(filedir , exist_ok=True )
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file:  {filepath}")
+    else: logging.info(f"{filename} is already exists")
